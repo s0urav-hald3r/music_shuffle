ce609258eaca1ef1a8a86c33670091dba41b15f4
s0urav-hald3r <bishalder96@gmail.com>
2024-11-27 20:57:56 +0530

    onboarding assets download

---
+++ lib/config/constants.dart
@@ -0,0 +1,5 @@
+// Image constants
+const String onboardingOneImage = 'assets/images/onboardingOne.png';
+const String onboardingTwoImage = 'assets/images/onboardingTwo.png';
+const String onboardingThreeImage = 'assets/images/onboardingThree.png';
+const String unlockPremium = 'assets/images/unlock_premium.png';
