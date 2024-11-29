f019f023e86b1d619398929e6ad99c9ace429b89
s0urav-hald3r <bishalder96@gmail.com>
2024-11-29 15:04:48 +0530

    song view UI complete

--- lib/views/home/songs_view.dart
+++ lib/views/home/songs_view.dart
@@ -31,13 +31,13 @@ class SongsView extends StatelessWidget {
             width: size.width,
             height: size.height,
             child: SafeArea(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: size.width,
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Row(
+              child: Container(
+                width: size.width,
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             InkWell(
@@ -51,37 +51,128 @@ class SongsView extends StatelessWidget {
                               child: SvgPicture.asset(upload),
                             ),
                           ]),
-                    ),
-                    const Spacer(),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 52,
-                      margin: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(40),
-                        gradient: elevated,
+                      const Spacer(),
+                      Text(
+                        '50 Songs',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12,
+                          color: whiteColor,
+                        ),
                       ),
-                      child: ElevatedButton(
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SvgPicture.asset(transfer),
-                              const SizedBox(width: 12),
-                              Text(
-                                'Transfer',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: whiteColor,
+                      Text(
+                        'Apple New Music',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28,
+                          color: whiteColor,
+                        ),
+                      ),
+                      Text(
+                        '9500 Gilman Drive La Jolla, CA',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                          color: whiteColor,
+                        ),
+                      ),
+                      const SizedBox(height: 24),
+                      Divider(
+                        height: 1,
+                        thickness: .5,
+                        color: whiteColor.withOpacity(.5),
+                      ),
+                      const SizedBox(height: 24),
+                      SizedBox(
+                        height: size.height / 2.5,
+                        child: ListView.separated(
+                          itemCount: 7,
+                          itemBuilder: (context, index) {
+                            return Row(children: [
+                              Container(
+                                width: 56,
+                                height: 56,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(4),
                                 ),
+                                clipBehavior: Clip.none,
+                                child: Image.asset(albumArt),
                               ),
-                            ]),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ]),
+                              const SizedBox(width: 12),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8),
+                                child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Walk It Off',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 16,
+                                          color: whiteColor,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Bill Weeds & Cory Weeds',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 14,
+                                          color: const Color(0xFFEBEBF5)
+                                              .withOpacity(.6),
+                                        ),
+                                      ),
+                                    ]),
+                              )
+                            ]);
+                          },
+                          separatorBuilder: (context, index) {
+                            return Divider(
+                              height: 16,
+                              thickness: .5,
+                              color: whiteColor.withOpacity(.5),
+                            );
+                          },
+                        ),
+                      )
+                    ]),
+              ),
             ),
-          )
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: SafeArea(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 52,
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  gradient: elevated,
+                ),
+                child: ElevatedButton(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(transfer),
+                        const SizedBox(width: 12),
+                        Text(
+                          'Transfer',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                            color: whiteColor,
+                          ),
+                        ),
+                      ]),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ),
         ]),
       ),
     );
